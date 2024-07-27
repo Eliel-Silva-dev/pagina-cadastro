2014b173b786ad9de322290fd1863721752cac9a
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-07-27 12:06:29 -0300

    feat: react cadastro service api auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,3 @@
-/*
 import {
   createContext,
   useCallback,
@@ -38,8 +37,39 @@ export const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
   }, []);
 
   const handleLogin = useCallback(async (email: string, password: string) => {
-    const result = await AuthService.
-  });
-  return <></>;
+    const result = await AuthService.auth(email, password);
+
+    if (result instanceof Error) {
+      return result.message;
+    } else {
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY_ACCESS_TOKEN,
+        JSON.stringify(result.accessToken),
+      );
+
+      setAccessToken(result.accessToken);
+    }
+  }, []);
+
+  const handleLogout = useCallback(() => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY_ACCESS_TOKEN);
+
+    setAccessToken(undefined);
+  }, []);
+
+  const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
-*/
+
+export const useAuthContext = () => useContext(AuthContext);
